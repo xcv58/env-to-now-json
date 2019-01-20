ed6f2411c99c5472c047c8a34d35f0ca24f06feb
xcv58 <i@xcv58.com>
2019-01-20 10:38:53 -0800

    Workable version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const dotenv = require('dotenv')
 const editJsonFile = require('edit-json-file')
+const yargs = require('yargs')
 
 const loadEnvFile = path => {
   const { error, parsed } = dotenv.config({ path })
@@ -36,12 +37,20 @@ const getOutputFile = argv => {
   return `now.${environment}.json`
 }
 
-require('yargs')
-  .command(
+const updateNowFile = (output, variables, environment) => {
+  const file = editJsonFile(output)
+  const env = file.env || {}
+  file.set('env',Object.assign({}, env,
+    ...Object.keys(variables).map(x => ({ [x]: `@${getSecretName(x, environment)}` }))
+  ))
+  file.save()
+}
+
+yargs.command(
     `$0 [env_file]`,
     'generate now.json config file',
-    yargs => {
-      yargs
+    args => {
+      args
         .positional('env_file', {
           alias: 'f',
           describe: 'the input .env file',
@@ -62,10 +71,11 @@ require('yargs')
       const { env_file, environment } = argv
       const variables = loadEnvFile(env_file)
       updateNowSecrets(variables, environment)
-      const output = getOutputFile(argv)
-      console.log(output)
-      const file = editJsonFile(`${__dirname}/${output}`)
-      file.save()
+      updateNowFile(
+        getOutputFile(argv),
+        variables,
+        environment
+      )
     },
   )
   .demandCommand()
